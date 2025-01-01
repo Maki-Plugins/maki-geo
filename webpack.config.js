4232d363710830c1ed2479e1a46eaa34668c8371
Jannes <jannes_elings@hotmail.com>
2025-01-01 21:21:28 +0100

    refactor: Simplify webpack config and use default entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const path = require('path');
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const path = require("path");
 
 module.exports = {
-    ...defaultConfig,
-    entry: {
-        'geo-popup': './src/blocks/geo-popup/geo-popup.js',
-        'popup-handler': './src/blocks/geo-popup/popup-handler.js',
-        'admin': './src/admin/index.js',
-        'components': './src/components/geo-rules/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].js'
-    }
+  ...defaultConfig,
+  entry: {
+    ...defaultConfig.entry(),
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+  },
 };
