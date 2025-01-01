fd62eaec1d27ff3d59e491d17a33ffad8bc17053
Jannes <jannes_elings@hotmail.com>
2025-01-01 15:34:07 +0100

    refactor: Rename location block function and add geo popup block function

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ if (! defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-function gu_create_location_block()
+function gu_create_geo_content_block()
 {
-    register_block_type(__DIR__ . '/build/blocks/location');
+    register_block_type(__DIR__ . '/build/blocks/geo-content');
 
     add_action('enqueue_block_assets', function () {
         // Inline script data
@@ -29,9 +29,29 @@ function gu_create_location_block()
         );
 
         // Add the inline script before your block's main script
-        wp_add_inline_script('geo-utils-location-block-editor-script', $script, 'before');
+        wp_add_inline_script('geo-utils-geo-content-block-editor-script', $script, 'before');
     });
 }
-add_action('init', 'gu_create_location_block');
+add_action('init', 'gu_create_geo_content_block');
+
+function gu_create_geo_popup_block()
+{
+    register_block_type(__DIR__ . '/build/blocks/geo-popup');
+
+    add_action('enqueue_block_assets', function () {
+        // Inline script data
+        $script = sprintf(
+            'window.geoUtilsData = %s;',
+            wp_json_encode([
+                'nonce' => wp_create_nonce('wp_rest'),
+                'endpoint' => rest_url('geoutils/v1/location')
+            ])
+        );
+
+        // Add the inline script before your block's main script
+        wp_add_inline_script('geo-utils-geo-popup-block-editor-script', $script, 'before');
+    });
+}
+add_action('init', 'gu_create_geo_popup_block');
 
 require_once("src/index.php");
