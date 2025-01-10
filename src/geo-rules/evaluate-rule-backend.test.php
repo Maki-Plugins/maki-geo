61eebb15014ab173af0b699df0807ebdc7a72600
Jannes <jannes_elings@hotmail.com>
2025-01-10 13:57:27 +0100

    test: Add evaluate-rule-backend test file for geo-rules

---
+++ src/geo-rules/evaluate-rule-backend.test.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Sample_Plugin
+ */
+
+/**
+ * Sample test case.
+ */
+class TestMaki extends WP_UnitTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * A single example test.
+     */
+    public function test_sample()
+    {
+        // Replace this with some actual testing code.
+        $this->assertTrue(true);
+    }
+}
