221c9590bbefff2d5dbffe4f90b066b9a183d5e5
Jannes <jannes_elings@hotmail.com>
2025-04-22 16:39:05 +0200

    fix: Fix geo redirection playwright tests

--- e2e-tests/redirection.test.ts
+++ e2e-tests/redirection.test.ts
@@ -11,17 +11,22 @@ test.describe("Geo redirection", () => {
     await page.getByRole("button", { name: " Geo redirection" }).click();
     await page.getByRole("button", { name: " Add new redirection" }).click();
     await page.getByLabel("Geo Redirect Name").fill(redirectionName);
-    await page.getByPlaceholder("Country or country code").fill("Afghanistan");
+    await page
+      .getByRole("textbox", { name: "Country or country code" })
+      .fill("Afghanistan");
     await page
       .getByLabel("Redirect URL")
       .fill("http://localhost:8888/redirection-success");
-    await page.getByRole("button", { name: "Review & save" }).click();
     await page.getByRole("button", { name: "Create Redirection" }).click();
     await expect(
       page.getByRole("heading", { name: redirectionName }).first(),
     ).toBeVisible();
     // Delete
-    await page.getByTestId("mgeo_delete_redirection").click();
+    page.on("dialog", async (dialog) => {
+      // Message will be "Are you sure you want to delete this redirection?"
+      await dialog.accept();
+    });
+    await page.getByTestId("mgeo_delete_redirection").first().click();
   });
 
   test("can edit redirection", async ({ page }) => {
@@ -29,24 +34,28 @@ test.describe("Geo redirection", () => {
     await page.getByRole("button", { name: " Geo redirection" }).click();
     await page.getByRole("button", { name: " Add new redirection" }).click();
     await page.getByLabel("Geo Redirect Name").fill(redirectionName);
-    await page.getByPlaceholder("Country or country code").fill("Afghanistan");
+    await page
+      .getByRole("textbox", { name: "Country or country code" })
+      .fill("Afghanistan");
     await page
       .getByLabel("Redirect URL")
       .fill("http://localhost:8888/redirection-success");
-    await page.getByRole("button", { name: "Review & save" }).click();
     await page.getByRole("button", { name: "Create Redirection" }).click();
     await expect(
       page.getByRole("heading", { name: redirectionName }).first(),
     ).toBeVisible();
     const changedRedirectionName = "Changed Geo Redirect Name";
-    await page.getByTestId("mgeo_edit_redirection").click();
+    await page.getByTestId("mgeo_edit_redirection").first().click();
     await page.getByLabel("Geo Redirect Name").fill(changedRedirectionName);
-    await page.getByRole("button", { name: "Review & save" }).click();
     await page.getByRole("button", { name: "Update Redirection" }).click();
     await expect(
       page.getByRole("heading", { name: changedRedirectionName }).first(),
     ).toBeVisible();
     // Delete
-    await page.getByTestId("mgeo_delete_redirection").click();
+    page.on("dialog", async (dialog) => {
+      // Message will be "Are you sure you want to delete this redirection?"
+      await dialog.accept();
+    });
+    await page.getByTestId("mgeo_delete_redirection").first().click();
   });
 });
